631fde3279e9d3a0c61d0af9d9e07f5bdc99d037
xieguaiwu <xieguaiwu@163.com>
2025-11-02 15:39:27 +0800

    Add files via upload

--- GL.cpp
+++ GL.cpp
@@ -6,6 +6,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+//custom getch function
 int getch() {
 	struct termios oldt, newt;
 	int ch;
@@ -23,10 +24,14 @@ int getch() {
 #include<string>
 #include<cstring>
 #include<cmath>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
 
 const unsigned char MAX_X = 100, MAX_Y = 100;
 
+char key;
 char c[MAX_X][MAX_Y]; //current
 char next_c[MAX_X][MAX_Y]; //intermedia map
 unsigned char dmap[MAX_X][MAX_Y]; //counter
@@ -44,19 +49,33 @@ void clear_screen() {
 
 void pause(bool screen);
 void notice();
-bool inp();
-void mony(int x);
+bool reset();
+bool reload_file();
+void simulate(int x);
 
 int main() {
+	bool isGoodLoad = false;
 	notice();
-	if (!inp())return 0;
-	mony(0);
+	key = getch();
+	switch (key) {
+	default:
+		break;
+	case 'R':
+	case 'r':
+		isGoodLoad = reload_file();
+		break;
+	}
+	if (!isGoodLoad) {
+		if (!reset())return 0;
+	}
+	simulate(0);
 	return 0;
 }
 
 void notice() {
 	cout << "Type \"+\" to create a new cell, and type \"-\" to create empty space.\n";
 	cout << "Set the map as follows:\n1)--++--\n2)-+-+--\n...\nn)+-++--\n";
+	cout << "Press 'R' to load a custom map file, or other keys to reset map within this program.\n";
 }
 
 void pause(bool screen = false) {
@@ -65,7 +84,7 @@ void pause(bool screen = false) {
 	if (screen)clear_screen();
 }
 
-void outp() {
+void print_map() {
 	for (int i = 1; i <= mapX; i++) {
 		for (int j = 1; j <= mapY; j++) {
 			if (c[i][j] == '+') cout << "⬜";
@@ -75,15 +94,23 @@ void outp() {
 	}
 }
 
-bool inp() {
+bool isGoodCharacter(int checkCharX, int checkCharY) {
+	if (c[checkCharX][checkCharY] != '-' && c[checkCharX][checkCharY] != '+') {
+		cout << "Illegal character!\n";
+		return false;
+	}
+	return true;
+}
+
+bool reset() {
 	cout << "Input the size of map.\nX: ";
-	// 修正: 检查输入是否成功
-	if (!(cin >> mapX)) return false;  
+	//prevent bad int
+	if (!(cin >> mapX)) return false;
 	cout << "\nY: ";
-	if (!(cin >> mapY)) return false;  
-	
+	if (!(cin >> mapY)) return false;
+
 	cout << "\nGame: " << mapX << " * " << mapY << "\n";
-	if (mapX > 100 || mapY > 100 || mapX == 0 || mapY == 0) { 
+	if (mapX > 100 || mapY > 100 || mapX == 0 || mapY == 0) {
 		cout << "x and y must be in the interval (0 , 100]\n";
 		return false;
 	}
@@ -91,27 +118,68 @@ bool inp() {
 	for (int i = 1; i <= mapX; i++) {
 		for (int j = 1; j <= mapY; j++) {
 			cin >> c[i][j];
-			if (c[i][j] != '-' && c[i][j] != '+') {
-				cout << "Illegal character!\n";
-				return false;
-			}
+			if (!isGoodCharacter(i, j))return false;
 		}
 	}
-	outp();
+	print_map();
+	pause(0);
+	return true;
+}
+
+bool reload_file() {
+	while (1) {
+		bool all_okay = true;
+		string file_name;
+		cout << "Input the file name of your custom map, type \"q!\" to quit:\n";
+		cin >> file_name;
+		if (file_name == "q!") return false;
+		ifstream reader(file_name);
+		// detect for errors
+		if (!reader.is_open()) {
+			cout << "No such file. Try again.\n";
+			all_okay = false;
+			continue;
+		}
+		// read dimensions first
+		if (!(reader >> mapX >> mapY) ||
+		        (mapX > MAX_X || mapY > MAX_Y || mapX == 0 || mapY == 0)) {
+			cout << "The x and y coordinates of the map must be in the interval (0 , 100]\n";
+			reader.close();
+			all_okay = false;
+			continue;
+		}
+		// then read the map to detect illegal character
+		for (int i = 1; i <= mapX; i++) {
+			for (int j = 1; j <= mapY; j++) {
+				reader >> c[i][j];
+				if (!isGoodCharacter(i, j)) {
+					cout << "Illegal character in map at position (" << i << ", " << j << ")\n";
+					reader.close();
+					all_okay = false;
+					break; // jump away
+				}
+			}
+			if (!all_okay) break;
+		}
+		if (!all_okay) continue;//start again from the beginning
+		else break;//now everything's fine
+	}
+	cout << "Game: " << mapX << " * " << mapY << "\n";
+	print_map();
 	pause(0);
 	return true;
 }
 
 void calculate_neighbors() {
-	memset(dmap, 0, sizeof(dmap)); 
+	memset(dmap, 0, sizeof(dmap));
 	//reset to zero
 	for (unsigned int x = 1; x <= mapX; x++) {
 		for (unsigned int y = 1; y <= mapY; y++) {
 			for (int i = 0; i < 8; i++) {
 				int tx = x + dx[i];
 				int ty = y + dy[i];
-				
-                // check boundary
+
+				// check boundary
 				if (tx >= 1 && tx <= mapX && ty >= 1 && ty <= mapY) {
 					if (c[tx][ty] == '+') {
 						dmap[x][y]++;
@@ -123,41 +191,41 @@ void calculate_neighbors() {
 }
 
 void apply_rules() {
-    for (unsigned int x = 1; x <= mapX; x++) {
+	for (unsigned int x = 1; x <= mapX; x++) {
 		for (unsigned int y = 1; y <= mapY; y++) {
 			unsigned char live_neighbors = dmap[x][y];
-			
-            next_c[x][y] = c[x][y]; 
-            
-			if (c[x][y] == '+') { // 活细胞
+
+			next_c[x][y] = c[x][y];
+
+			if (c[x][y] == '+') { //living cells
 				if (live_neighbors < 2 || live_neighbors > 3) {
 					next_c[x][y] = '-';
 				}
-			} else { // 死细胞
+			} else { //dead cells
 				if (live_neighbors == 3) {
 					next_c[x][y] = '+';
 				}
 			}
 		}
 	}
-    
-    for (unsigned int x = 1; x <= mapX; x++) {
+
+	for (unsigned int x = 1; x <= mapX; x++) {
 		for (unsigned int y = 1; y <= mapY; y++) {
-            c[x][y] = next_c[x][y];
-        }
-    }
+			c[x][y] = next_c[x][y];
+		}
+	}
 }
 
-void mony(int generation) {
-	while(true) { // 无限循环模拟
+void simulate(int generation) {
+	while (1) { //main loop for simulation
 		clear_screen();
 		cout << "Generation: " << generation << "\n";
-		outp();
-		calculate_neighbors(); 
-		apply_rules();         
+		print_map();
+		calculate_neighbors();
+		apply_rules();
 		cout << " (Press 'q' to quit, or any other key to continue...)\n";
 		generation++;
-        char key = getch();
-        if (key == 'q' || key == 'Q') break;
+		key = getch();
+		if (key == 'q' || key == 'Q') break;
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Game_Of_Life
-Conway's game of life in C++, running on both Linux and Windows.
+A terminal-based Conway's game of life, written in C++, running on both Linux and Windows.
+
+---
+
+## Features
+
+- Light-weight
+- You can load your custom map file easily
+
+---
+
+## Tutorial
+
+When editing your map, just remember that:
+
+- "+" stands for a living cell
+- "-" stands for an empty block
+- Both the x and y coordinate of the game map must be in the interval of (0, 100]
+- Your custom map file must be in pure text format
+- Remember to state the length and width of your custom map at the beginning your map file
+- check for "example_map.txt" if you need help on creating a custom map file
+
+---
+
+## Screenshots
+
+![in_terminal](./screenshots/screenshot_2.png)
+
+![display](./screenshots/screenshot_1.png)
+
+
